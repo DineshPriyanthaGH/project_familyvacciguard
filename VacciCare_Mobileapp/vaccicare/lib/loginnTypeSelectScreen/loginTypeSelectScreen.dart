f1d84b2bc77d8d9d3d000d68f98a2bf3af361ca0
Dinesh Priyantha <dineshpriyantha200248@gmail.com>
2024-05-09 23:36:04 +0530

    create LoginTypeSelectScreen class

--- VacciCare_Mobileapp/vaccicare/lib/loginnTypeSelectScreen/loginTypeSelectScreen.dart
+++ VacciCare_Mobileapp/vaccicare/lib/loginnTypeSelectScreen/loginTypeSelectScreen.dart
@@ -1,2 +1,8 @@
 import 'package:flutter/material.dart';
 import '../../core/app_export.dart';
+
+class LoginTypeSelectScreen extends StatelessWidget {
+  const LoginTypeSelectScreen({Key? key})
+      : super(
+          key: key,
+        );
