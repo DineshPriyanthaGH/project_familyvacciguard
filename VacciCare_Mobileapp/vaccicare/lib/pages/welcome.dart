45078effd59d6f0e1119f60f904580e401e9ec45
Dinesh Priyantha <dineshpriyantha200248@gmail.com>
2024-06-09 16:54:48 +0530

    BoxDecoration in welcome page

--- VacciCare_Mobileapp/vaccicare/lib/pages/welcome.dart
+++ VacciCare_Mobileapp/vaccicare/lib/pages/welcome.dart
@@ -2,3 +2,20 @@ import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:vaccicare/utils.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Welcome extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+    return
+    Container (
+      child: Container(
+        decoration:BoxDecoration(
+          color:Color(0xFFFFFFFF),
+          borderRadius: BorderRadius.circular(20),
+        ),
+      )
+    )
+  }
+  
+}
