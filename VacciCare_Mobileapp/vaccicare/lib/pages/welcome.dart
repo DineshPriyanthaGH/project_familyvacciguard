6a42e40ae16cc818c53430522789f9f41c496ab8
Dinesh Priyantha <dineshpriyantha200248@gmail.com>
2024-06-10 22:40:51 +0530

    add assets folder to flutter, welocome page  and errors fix

--- VacciCare_Mobileapp/vaccicare/lib/pages/welcome.dart
+++ VacciCare_Mobileapp/vaccicare/lib/pages/welcome.dart
@@ -4,18 +4,130 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:vaccicare/utils.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Welcome extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return
-    Container (
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       child: Container(
-        decoration:BoxDecoration(
-          color:Color(0xFFFFFFFF),
+        decoration: BoxDecoration(
+          color: Color(0xFFFFFFFF),
           borderRadius: BorderRadius.circular(20),
         ),
-      )
-    )
+        child: SizedBox(
+          width: 428,
+          height: 904,
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: -22,
+                child: Container(
+                  width: 428,
+                  height: 926,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFFFFF),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          right: 0,
+                          top: 0,
+                          bottom: 0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Color(0xFF0B153C),
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage(
+                                  'assets/images/unsplash_sf_dofj_xtx_he.jpeg',
+                                ),
+                              ),
+                            ),
+                            child: Container(
+                              width: 428,
+                              height: 926,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Container(
+                            padding: EdgeInsets.fromLTRB(0, 272, 0, 441),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.fromLTRB(0, 0, 0.2, 15.6),
+                                  width: 140.4,
+                                  height: 153.4,
+                                  child: SizedBox(
+                                    width: 140.4,
+                                    height: 153.4,
+                                    child: SvgPicture.asset(
+                                      'assets/vectors/employees_are_vaccinated_ecommerce_employees_are_vaccinated_4_x2.svg',
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  child: Text(
+                                    'VacciCare',
+                                    style: GoogleFonts.getFont(
+                                      'Montserrat',
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 36,
+                                      color: Color(0xFFFFFFFF),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                right: -2241,
+                top: 198,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF5F5F5),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.contain,
+                          image: AssetImage(
+                            'assets/images/apple_logo.png',
+                          ),
+                        ),
+                      ),
+                      child: Container(
+                        width: 30,
+                        height: 30,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-  
 }
