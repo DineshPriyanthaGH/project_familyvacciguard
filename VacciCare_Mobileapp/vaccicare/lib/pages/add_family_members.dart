9daeb16de10bf4cb192a44a71eaf05d94fc17266
Hasikodithuwakku <hasinichamalka5@gmail.com>
2024-06-12 17:40:01 +0530

    add class to add familiy members  sucsessfully

--- VacciCare_Mobileapp/vaccicare/lib/pages/add_family_members.dart
+++ VacciCare_Mobileapp/vaccicare/lib/pages/add_family_members.dart
@@ -3,3 +3,18 @@ import 'dart:ui';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_app/utils.dart';
 import 'package:google_fonts/google_fonts.dart';
+class AddFamilyMembersSuccessful extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return 
+    Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Color(0xFFFFFFFF),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(
+            'assets/images/about_tetanus_toxoid.jpeg',
+          ),
+        ),
+      ),
