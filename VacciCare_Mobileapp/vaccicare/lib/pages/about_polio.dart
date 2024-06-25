583b72025ed3b3fb528c15d9cac1129da386632c
DineshPriyanthaGH <Priyanthadinesh2002@gmail.com>
2024-06-25 23:11:04 +0530

    up: import libarires

--- VacciCare_Mobileapp/vaccicare/lib/pages/about_polio.dart
+++ VacciCare_Mobileapp/vaccicare/lib/pages/about_polio.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vaccicare/utils.dart';
+import 'package:google_fonts/google_fonts.dart';
